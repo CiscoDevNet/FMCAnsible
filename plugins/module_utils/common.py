44933d34aa8704c8e5b1fbaf85aee21305acfcc3
Joe Agster <joe.agster@cdw.com>
2022-04-26 09:05:05 -0700

    Fix delete and handling of paging items

--- plugins/module_utils/common.py
+++ plugins/module_utils/common.py
@@ -188,28 +188,6 @@ def equal_values(v1, v2):
     else:
         return v1 == v2
 
-# meignw2021
-# def equal_objects(d1, d2):
-#     """
-#     Checks whether two objects are equal. Ignores special object properties (e.g. 'id', 'version') and
-#     properties with None and empty values. In case properties contains a reference to the other object,
-#     only object identities (ids and types) are checked. Also, if an array field contains multiple references
-#     to the same object, duplicates are ignored when comparing objects.
-
-#     :type d1: dict
-#     :type d2: dict
-#     :return: True if passed objects and their properties are equal. Otherwise, returns False.
-#     """
-
-#     def prepare_data_for_comparison(d):
-#         d = dict((k, d[k]) for k in d.keys() if k not in NON_COMPARABLE_PROPERTIES and d[k])
-#         d = delete_ref_duplicates(d)
-#         return d
-
-#     d1 = prepare_data_for_comparison(d1)
-#     d2 = prepare_data_for_comparison(d2)
-#     return equal_dicts(d1, d2, compare_by_reference=False)
-
 
 def equal_objects(d1, d2, compare_common_fields_only=True):
     """
