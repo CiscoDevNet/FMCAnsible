2e271ba0bc57cb3bf38a92edb2af433068daa7fe
jeroenwittock <jeroenwittock@gmail.com>
2025-07-01 17:23:39 +0200

    fixing linting issue in device.py

--- plugins/module_utils/device.py
+++ plugins/module_utils/device.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from enum import Enum
 from urllib.parse import urlparse
 
