86978c642638dc39cab57eaabf240764c73fb236
jeroenwittock <jeroenwittock@gmail.com>
2025-07-03 17:31:38 +0200

    bugfix in: Process each rule to get nested objects

--- plugins/modules/fmc_access_policies.py
+++ plugins/modules/fmc_access_policies.py
@@ -19,6 +19,8 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+# TODO: Create feature testing for this module to ensure it works as expected.
+
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
